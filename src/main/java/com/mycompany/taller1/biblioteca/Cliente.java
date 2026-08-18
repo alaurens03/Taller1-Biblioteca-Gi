37ad96355ffa8940ec40c59906bb174e31ec994d
Andrea Laurens <alaurens@unicesar.edu.co>
2026-08-17 23:49:13 -0500

    feat: crear clase Cliente heredando de Persona

---
+++ src/main/java/com/mycompany/taller1/biblioteca/Cliente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca;
+
+/**
+ *
+ * @author PC USER
+ */
+public class Cliente extends Persona {
+
+    private String email;
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(String id, String nombre, String telefono, String email) {
+        super(id, nombre, telefono);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + super.toString() + ", email=" + email + "}";
+    }
+}
